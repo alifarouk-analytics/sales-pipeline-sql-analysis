4d5df9e916566bffd50b4475d0644e53d7e0504a
alifarouk-analytics <ali.m.farouk@gmail.com>
2026-04-07 12:16:31 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,21 +1,36 @@
+-- =========================================
+-- 1. Total Deals & Revenue
+-- =========================================
 Select
 	Count(*) AS total_deals,
     Sum(close_value) as total_revenue
 From crm_sales_pipeline_analysis
 
+-- =========================================
+-- 2. Won deals
+-- =========================================
 Select *
 From crm_sales_pipeline_analysis
 Where deal_stage = 'won';
 
+-- =========================================
+-- 3. Win Rate
+-- =========================================
 Select
 	Count(Case When deal_stage = 'won' Then 1 end) * 1 / Count(*) AS win_rate
 From crm_sales_pipeline_analysis
 
+-- =========================================
+-- 4. Average Deal Size
+-- =========================================
 Select
 	Avg(close_value) as avg_deal_size
 From crm_sales_pipeline_analysis
 Where deal_stage = 'Won';
 
+-- =========================================
+-- 5. Best Performing Sales Agents
+-- =========================================
 Select
 	sales_agent,
     Sum(close_value) as Revenue
@@ -24,6 +39,9 @@ Where deal_stage = 'won'
 Group By sales_agent
 order by Revenue Desc
 
+-- =========================================
+-- 6. Deals by Stage
+-- =========================================
 Select
 	deal_stage,
     Count(*) AS num_deals
@@ -31,14 +49,9 @@ From crm_sales_pipeline_analysis
 Group by deal_stage
 Order by num_deals Desc;
 
-Select
-	Date_Trunc('month', close_date) as month,
-    Sum(close_value) as revenue
-From crm_sales_pipeline_analysis
-Where deal_stage = 'Won'
-Group by month
-Order by month;
-
+-- =========================================
+-- 7. Sales Conversion by Stage
+-- =========================================
 Select
 	deal_stage,
     Count(*) AS deals,
